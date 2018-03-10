161b14ec85c26297629dd519836c26503ceac9cb
unknown <henrytanjaya11@gmail.com>
2018-03-10 23:21:10 +0700

    init

---
+++ models/campground.js
@@ -0,0 +1,29 @@
+var mongoose   = require('mongoose');
+
+var campgroundSchema=new mongoose.Schema({
+	name: String,
+	image : String,
+	description : String,
+	createdAt: { type: Date, default: Date.now },
+	author : {
+		id: {
+			type : mongoose.Schema.Types.ObjectId,
+			ref : "User"
+			// model yang kita connect
+		},
+		username : String,
+		image : String
+	},
+	comments : [
+		{
+			type : mongoose.Schema.Types.ObjectId,
+			ref : "Comment"
+			// model yang kita connect
+		}
+
+	]
+});
+
+var Campground= mongoose.model("Campground",campgroundSchema);
+
+module.exports= Campground;
